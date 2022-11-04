6d8003b63279515a3e65ddd21c9db459b9aa3f83
Jordan Sinou <sinoujordan@gmail.com>
2022-11-04 13:22:31 +0100

    correction

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,13 +1,58 @@
 const User = require('../models/userModel');
 
-const index = (req, res) => {
+const index = (req, res) => { // Récupérer les users et les afficher dans la vue
+    User.find((err, users) => {
+        res.status(200).render('users', { users })
+    })
+}
+
+const create = (req, res) => { // Créer un user
     const { name, age, city } =  req.body
 
     User.create({ name, age, city })
-        .then(user => res.json(user))
-        .catch(err => res.json(err))
+        .then(user => {
+            res.status(201).json(user)
+        })
+        .catch(err => res.status(500).json(err))
 }
 
-module.exports = {
-    index
+const findUser = (req, res) => { // Récupérer un user par son id
+    const {id} = req.params
+
+    User.findById(id, (err, user) => {
+
+        if (err) res.status(500).json({err, message: "Unable to find user"}) // Si erreur, renvoyer un code 500 et l'erreur
+        if (user === null) res.status(404).json({message: "user not found"}) // Si pas de user, renvoyer un code 404 et un message
+
+        else res.status(200).json(user)
+    })
+}
+
+const deleteUser = (req, res) => { // Supprimer un user par son id
+    const {id} = req.params
+
+    User.findByIdAndDelete(id, (err, user) => {
+        if (err) res.status(500).json({err, message: "Unable to delete user"}) // Si erreur, renvoyer un code 500 et l'erreur
+        if (user === null) res.status(404).json({message: "user not found"}) // Si pas de user, renvoyer un code 404 et un message
+        else res.status(200).json({message: "user deleted", user})
+    })
+}
+
+const editUser = (req, res) => { // edit un user par son id
+    const {id} = req.params
+    const {name, age, city} = req.body
+
+    User.findByIdAndUpdate(id, {name, age, city}, (err, user) => {
+        if (err) res.status(500).json({err, message: "Unable to update user"}) // Si erreur, renvoyer un code 500 et l'erreur
+        if (user === null) res.status(404).json({message: "user not found"}) // Si pas de user, renvoyer un code 404 et un message
+        else res.status(200).json(user)
+    })
+}
+
+module.exports = { // Exporter les méthodes
+    index,
+    create,
+    findUser,
+    deleteUser,
+    editUser
 }
--- routes/users.js
+++ routes/users.js
@@ -2,15 +2,14 @@ var express = require('express');
 var router = express.Router();
 const userController = require('../controllers/userController');
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+// Toutes les routes users commence par /users (voir app.js ligne 8)
 
-router.post('/', userController.index);
+router.get('/', userController.index);
+router.post('/', userController.create);
+router.delete('/:id', userController.deleteUser);
+router.put('/:id', userController.editUser);
+router.get('/:id', userController.findUser) // :id est un paramètre, on peut y accéder avec req.params.id dans le controller
 
-router.get('/about', function(req, res, next) {
-  res.send('about user');
-});
+
 
 module.exports = router;
